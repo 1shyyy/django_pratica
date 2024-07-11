e4cd141b810167a810307ba95c8734a6c3df7e67
Gabriel Braga <bragagabriel843@gmail.com>
2024-07-11 19:22:50 -0300

    aula 3

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Teste')
-
-
-def contato(request):
-    return HttpResponse('Contato')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
+    return render(request, 'recipes/home.html')
